fdb9a0574bd0617d37a31a922ebc073d84c4aa7d
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2018-12-27 18:33:04 +0900

    feat: Create selfIntroduction Page

--- src/components/selfIntroduction/self_introduction.ts
+++ src/components/selfIntroduction/self_introduction.ts
@@ -9,9 +9,9 @@ import PageTitle from "../parts/pageTitle/PageTitle.vue";
 })
 export default class Introduction extends Vue {
   pageTitleText: string = "自己紹介";
-  nameText: string[] = ["Ryosuke Izumi", "is_ryo"];
-  jobsText: string[] = ["IoT", "FrontEnd"];
-  communitiesText: string[] = ["v-kansai", "JAWS-UG", "Node学園"];
+  nameText: string = "Ryosuke Izumi / is_ryo";
+  jobText: string = "IoT / FrontEnd";
+  communityText: string = "v-kansai / JAWS-UG / Node学園";
   transitionPageInfo: transitionPageInfoType = {
     preb: "/",
     next: "/"
